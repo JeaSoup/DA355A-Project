550807f16abade578abedfd45ddddc7bd519401e
Jesper Lindberg <jesperlindberg91@gmail.com>
2020-05-22 07:50:53 +0200

    finale version for picture mode

--- testing/script.js
+++ testing/script.js
@@ -1,17 +1,42 @@
-console.log("Hej");
+$('.status').text("Loading model, please wait..");
 
-
+let cocoModel;
 const coco = cocoSsd.load();
-const image = $('#image');
-console.log(image[0])
-
-// Load the model.
 coco.then(model => {
-  console.log("trying...")
-  model.detect(image[0]).then(predictions => {
-    console.log('Predictions: ', predictions);
-    console.log(predictions[0].class);
+  $('.status').text("Model loaded ready to make prediction!");
+   cocoModel = model;
+});
+let image = new Image();
+console.log(src);
+//image = document.getElementById('image');
+//image.src = "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg"
+
+let prediction;
+
+
+
+$('#recognize').click(function() {
+  let src = $('#src').val();
+  image.src = src;
+  console.log(image.src)
+  cocoModel.detect(image).then(predictions => {
+    prediction = predictions[0];
+    console.log(prediction);
     $('.prediction').append(predictions[0].class);
+
+    const x = prediction.bbox[0];
+    const y = prediction.bbox[1];
+    const width = prediction.bbox[2];
+    const height = prediction.bbox[3];
+
+    const canvas = document.getElementById("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(x, y, width, height);
+
 
   });
 });
