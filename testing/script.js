c07a257230ce1eb3f163a095a3d8adf24a9c6d2d
Jesper Lindberg <jesperlindberg91@gmail.com>
2020-05-22 11:34:10 +0200

    add webcam feature

--- testing/script.js
+++ testing/script.js
@@ -1,7 +1,10 @@
 $('.status').text("Loading model, please wait..");
+const pictureCanvas = document.getElementById("canvas-picture");
+const videoCanvas = document.getElementById("canvas-video");
 
 let cocoModel;
 let image = new Image();
+let video = document.createElement('video');
 image.crossOrigin = "anonymous";
 const coco = cocoSsd.load();
 coco.then(model => {
@@ -10,34 +13,56 @@ coco.then(model => {
 });
 
 
+function renderPredictions(predictions, current) {
+  const ctx = current.getContext("2d");
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // Font options.
+  const font = "25px sans-serif";
+  ctx.font = font;
+  ctx.textBaseline = "top";
+  predictions.forEach(prediction => {
+    const x = prediction.bbox[0];
+    const y = prediction.bbox[1];
+    const width = prediction.bbox[2];
+    const height = prediction.bbox[3];
+    // Draw the bounding box.
 
+    pictureCanvas.width = image.width;
+    pictureCanvas.height = image.height;
+    videoCanvas.width = 600;
+    videoCanvas.height = 600;
+    //for image
+    ctx.drawImage(image, 0, 0, pictureCanvas.width, pictureCanvas.height);
+    //for video
+    ctx.drawImage(video, 0, 0), videoCanvas.width, videoCanvas.height;
+    ctx.strokeRect(x, y, width, height);
+    // Draw the label background.
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10); // base 10
+  });
+
+  predictions.forEach(prediction => {
+    const x = prediction.bbox[0];
+    const y = prediction.bbox[1];
+    // Draw the text last to ensure it's on top.
+    ctx.fillText(prediction.class, x, y);
+  });
+}
 
 function fetchPrediction() {
   let prediction;
 
   cocoModel.detect(image).then(predictions => {
-    prediction = predictions[0];
     console.log(prediction);
     $('.prediction').append(predictions[0].class);
 
-    const x = prediction.bbox[0];
-    const y = prediction.bbox[1];
-    const width = prediction.bbox[2];
-    const height = prediction.bbox[3];
-
-    const canvas = document.getElementById("canvas-picture");
-    canvas.width = image.width;
-    canvas.height = image.height;
-    const ctx = canvas.getContext("2d");
-
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(x, y, width, height);
+    renderPredcitions(predictions, canvas);
 
 
   });
 }
 
-$('#recognize').click(function() {
+$('#recognize-picture').click(function() {
   let src = $('#src').val();
   image.src = src;
   image.onload = function() {
@@ -46,7 +71,11 @@ $('#recognize').click(function() {
 
 });
 
-const video = document.getElementById("video")
+$('#recognize-video').click(function() {
+  detectFrame();
+
+});
+
 
 navigator.mediaDevices
   .getUserMedia({
@@ -62,13 +91,13 @@ navigator.mediaDevices
     video.onloadedmetadata = () => {
       video.play()
     }
-  }).catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
+  }).catch(function(err0r) {
+    console.log("Something went wrong!");
+  });
 
 function detectFrame() {
-  model.detect(video).then(predictions => {
-    renderOurPredictions(predictions)
+  cocoModel.detect(video).then(predictions => {
+    renderPredictions(predictions, videoCanvas)
     requestAnimationFrame(detectFrame)
   })
 }
