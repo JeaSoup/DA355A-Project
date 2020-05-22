770bc63e54ae7aa15be5e87d41e01ebc08251eea
Jesper Lindberg <jesperlindberg91@gmail.com>
2020-05-22 06:51:58 +0200

    Add prediction to show, using JQuery

--- testing/script.js
+++ testing/script.js
@@ -2,14 +2,16 @@ console.log("Hej");
 
 
 const coco = cocoSsd.load();
-const image = document.getElementById('image');
+const image = $('#image');
+console.log(image[0])
 
 // Load the model.
 coco.then(model => {
   console.log("trying...")
-  model.detect(image).then(predictions => {
+  model.detect(image[0]).then(predictions => {
     console.log('Predictions: ', predictions);
     console.log(predictions[0].class);
+    $('.prediction').append(predictions[0].class);
 
   });
 });
