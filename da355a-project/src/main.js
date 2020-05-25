adbd75276094fa13c462f2166a582ff36045d577
Christina3107 <christina.knepper@gmail.com>
2020-05-25 15:44:46 +0200

    language identification added

--- da355a-project/src/main.js
+++ da355a-project/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default-dark.css'
+import VueGeolocation from 'vue-browser-geolocation'
+import axios from 'axios'
 
-
+Vue.prototype.$http = axios
+Vue.use(VueGeolocation);
 Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
