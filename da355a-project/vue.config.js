07f84ae852e79bd0722a76145671c28b36e66573
Jesper Lindberg <jesperlindberg91@gmail.com>
2020-05-27 01:00:00 +0200

    fix mobile button visibility

--- da355a-project/vue.config.js
+++ da355a-project/vue.config.js
@@ -1,8 +1,7 @@
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
 
 module.exports = {
   // For building, publicPath is needed.
 
   publicPath: 'https://webshare.mah.se/ai8538/da355a/project/dist/'
-  
+
 }
