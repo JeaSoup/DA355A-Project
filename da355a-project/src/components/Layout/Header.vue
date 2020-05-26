080c668f01aade3f4f7649b4d8cb493be96d838b
Jesper Lindberg <jesperlindberg91@gmail.com>
2020-05-27 01:08:17 +0200

    change fontsize for mobile

--- da355a-project/src/components/Layout/Header.vue
+++ da355a-project/src/components/Layout/Header.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="md-layout md-alignment-center">
-    <span id="logo" class="md-display-4">Depicter</span>
-  </div>
+<div class="md-layout md-alignment-center">
+  <span id="logo" class="md-display-4">Depicter</span>
+</div>
 </template>
 
 <script>
 export default {
-    name: "Header"
+  name: "Header"
 }
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');
+
 div {
   padding: 20px;
-  border-bottom:2px solid white;;
+  border-bottom: 2px solid white;
+  ;
 }
 
-.md-display-4 >>> {
+@media only screen and (max-width: 600px) {
+  .md-display-4>>> {
+    font-size:  90px;
+  }
+}
+
+.md-display-4>>> {
   color: white !important;
   font-family: 'Acme', sans-serif;
 }
